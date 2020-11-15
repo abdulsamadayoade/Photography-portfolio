0c8e3b6702be708d15180aa89e554cb91e29c1c8
AbdulSamad Ayoade <ayoadesamad@gmail.com>
2020-11-15 21:54:06 +0100

    Code the menu toggler

---
+++ app.js
@@ -0,0 +1,6 @@
+// HAMBURGER MENU ON MOBILE DEVICES
+const menu = document.querySelector('.navbar-toggler');
+
+menu.addEventListener('click', function () {
+    menu.classList.toggle('menu-open');
+})
